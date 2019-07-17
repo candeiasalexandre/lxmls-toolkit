found bug inside my backprop, still dont work

lxmls/deep_learning/numpy_models/mlp.py
@@ -102,7 +102,7 @@ class NumpyMLP(MLP):
             W, b = self.parameters[n]
             
             if n == num_layers -1:
-                last_error = log_prob_y - index2onehot(output, np.unique(output).shape[0]) + 0.0
+                last_error = np.exp(log_prob_y) - index2onehot(output, np.unique(output).shape[0]) + 0.0
             else:
                 last_error = np.multiply(last_error, np.multiply(layer_inputs[n-1], 1.0-layer_inputs[n-1]))
 
@@ -110,7 +110,7 @@ class NumpyMLP(MLP):
             W_gradient = (-1.0/num_examples) * np.matmul(last_error.T, layer_inputs[n]) + 0.0
             b_gradient = (-1.0/num_examples) * np.sum(last_error, axis=0) + 0.0
             
-            #ipdb.set_trace()
+            ipdb.set_trace()
             last_error = np.matmul(W.T, last_error.T).T
             errors.append(last_error + 0.0)
             gradients.append((W_gradient, b_gradient))